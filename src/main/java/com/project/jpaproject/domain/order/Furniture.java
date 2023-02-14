cbcf2a9c43e94b59308678d7a97ca45d3a76861b
haegyeong <haegyeong.kim01@gmail.com>
2023-02-14 14:42:07 +0900

    고급매핑- 상속관계매핑

--- src/main/java/com/project/jpaproject/domain/order/Furniture.java
+++ src/main/java/com/project/jpaproject/domain/order/Furniture.java
@@ -1,11 +1,13 @@
 package com.project.jpaproject.domain.order;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@DiscriminatorValue("Furniture")
 @Entity
 public class Furniture extends Item{
     private int width;
--- src/main/java/com/project/jpaproject/domain/order/Item.java
+++ src/main/java/com/project/jpaproject/domain/order/Item.java
@@ -10,7 +10,8 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
 @Table(name = "item")
 public abstract class Item {
 
