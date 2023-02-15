4cf3cd0aa9c4eac5cca3b6e28ab67947e7a54cdb
haegyeong <haegyeong.kim01@gmail.com>
2023-02-15 13:06:14 +0900

    오류 수정

---
+++ src/main/java/com/project/jpaproject/domain/order/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.project.jpaproject.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, String> {
+
+
+
+
+}
--- src/test/java/com/project/jpaproject/domain/order/ProxyTest.java
+++ src/test/java/com/project/jpaproject/domain/order/ProxyTest.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,7 +44,7 @@ public class ProxyTest {
         // 회원 엔티티
         Member member = new Member();
         member.setName("kanghonggu");
-        member.setNickName("guppy.HAHA");
+        member.setNickName("guppy.ki");
         member.setAge(33);
         member.setAddress("서울시 동작구");
         member.setDescription("KDT 화이팅");
@@ -72,32 +71,32 @@ public class ProxyTest {
      * 영속성 정의하지 않으면 OrderItem에 대한 insert문이 없다. addOrderItems() 를 호출했음에도 불구하고
      * orderItem이 영속화가 같이 되지 않고 준영속 상태로 남아있게 된다.
      */
-    @Test
-    @DisplayName("영속성 전이")
-    void move_persist() {
-        EntityManager entityManager= emf.createEntityManager();
-        EntityTransaction transaction = entityManager.getTransaction();
-
-        Order order = entityManager.find(Order.class, uuid);
-
-        transaction.begin();
-
-        OrderItem orderItem = new OrderItem();   //준영속 상태
-        orderItem.setQuantity(10);
-        orderItem.setPrice(1000);
-
-        order.addOrderItems(orderItem); //order class에서 orderItems를 cascade하여 commit 하면 영속전이를 통해 영속으로 바뀌고 쿼리가 날라간다.
-
-        transaction.commit();   //flush()   //영속성 정의 X ordrItem: 쿼리 날라가지 않음
-
-        entityManager.clear();
-
-        //////////Orphan Test start
-        Order order2 = entityManager.find(Order.class, uuid);
-
-        transaction.begin();
-        order2.getOrderItems().remove(0);   //고아상태 flush 순간 RDS 에서도 삭제하겠다.   //orphan 속성 추가해야함 (ORder.class에서)
-        transaction.commit();
-    }
+//    @Test
+//    @DisplayName("영속성 전이")
+//    void move_persist() {
+//        EntityManager entityManager= emf.createEntityManager();
+//        EntityTransaction transaction = entityManager.getTransaction();
+//
+//        Order order = entityManager.find(Order.class, uuid);
+//
+//        transaction.begin();
+//
+//        OrderItem orderItem = new OrderItem();   //준영속 상태
+//        orderItem.setQuantity(10);
+//        orderItem.setPrice(1000);
+//
+//        order.addOrderItems(orderItem); //order class에서 orderItems를 cascade하여 commit 하면 영속전이를 통해 영속으로 바뀌고 쿼리가 날라간다.
+//
+//        transaction.commit();   //flush()   //영속성 정의 X ordrItem: 쿼리 날라가지 않음
+//
+//        entityManager.clear();
+//
+//        //////////Orphan Test start
+//        Order order2 = entityManager.find(Order.class, uuid);
+//
+//        transaction.begin();
+//        order2.getOrderItems().remove(0);   //고아상태 flush 순간 RDS 에서도 삭제하겠다.   //orphan 속성 추가해야함 (ORder.class에서)
+//        transaction.commit();
+//    }
 
 }
